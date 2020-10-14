ad50d6311d930e09e77afdda2ad3bc48f21b21b0
masaun <masanoriuno@gmail.com>
2020-10-14 23:59:02 +0900

    Add createAuthToken()

--- contracts/NftAuthToken.sol
+++ contracts/NftAuthToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.16;
+pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -11,6 +11,19 @@ contract NftAuthToken is ERC721 {
     uint public currentAuthTokenId;
 
     constructor() public ERC721("NFT Auth Token", "NAT") {}
+
+    function createAuthToken(address to, string memory authTokenURI) public returns (uint _newAuthTokenId) {
+        _createAuthToken(to, authTokenURI);
+    }
+
+    function _createAuthToken(address to, string memory authTokenURI) internal returns (uint _newAuthTokenId) {
+        uint newAuthTokenId = getNextAuthTokenId();
+        currentAuthTokenId++;
+        _mint(to, newAuthTokenId);
+        _setTokenURI(newAuthTokenId, authTokenURI);
+
+        return newAuthTokenId;
+    }
 
 
     /***
