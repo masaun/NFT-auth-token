ff8bf30353b7a09849254ab2e531179cf171e113
masaun <masanoriuno@gmail.com>
2020-10-15 00:08:31 +0900

    Add NftAuthTokenManager.sol (include deposit/stake())

---
+++ contracts/NftAuthTokenManager.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import { NftAuthToken } from "./NftAuthToken.sol";
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+
+
+contract NftAuthTokenManager {
+    using SafeMath for uint;
+
+    constructor() public {}
+
+    /***
+     * @notice - Create new NftAuthToken contract address
+     **/
+    function createNftAuthToken() public returns (address _nftAuthToken) {
+        NftAuthToken nftAuthToken = new NftAuthToken();  /// [Note]: There is no constructor
+        return address(nftAuthToken);
+    }
+
+    /***
+     * @notice - Deposit (Stake) DAI into the specified NftAuthToken contract address
+     **/
+    function depositDaiIntoNftAuthToken(uint depositAmount) public returns (bool) {
+        /// In progress
+    }
+    
+}
