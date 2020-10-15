ad4cc5b6dc86ae3b63a91a2d5e2d4c4d71331e84
masaun <masanoriuno@gmail.com>
2020-10-15 09:02:57 +0900

    Update depositDaiIntoNftAuthToken() by adding dai.transfer

--- contracts/NftAuthTokenManager.sol
+++ contracts/NftAuthTokenManager.sol
@@ -3,6 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import { NftAuthToken } from "./NftAuthToken.sol";
 
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 
@@ -12,7 +13,11 @@ contract NftAuthTokenManager {
 
     address[] nftAuthTokenList;
 
-    constructor() public {}
+    IERC20 public dai;
+
+    constructor(address _dai) public {
+        dai = IERC20(_dai);    
+    }
 
     /***
      * @notice - Create new NftAuthToken contract address
@@ -32,8 +37,10 @@ contract NftAuthTokenManager {
     /***
      * @notice - Deposit (Stake) DAI into the specified NftAuthToken contract address
      **/
-    function depositDaiIntoNftAuthToken(uint depositAmount) public returns (bool) {
-        /// In progress
+    function depositDaiIntoNftAuthToken(address _nftAuthToken, uint depositAmount) public returns (bool) {
+        /// Deposit DAI from msg.sender to specified NftAuthToken contract address via this contract address 
+        dai.transferFrom(msg.sender, address(this), depositAmount);
+        dai.transfer(_nftAuthToken, depositAmount);
     }
     
 }
