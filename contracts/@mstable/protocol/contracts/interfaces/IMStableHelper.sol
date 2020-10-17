4b7a2a094350fed55922fdf378051ae0018bf51b
masaun <masanoriuno@gmail.com>
2020-10-17 21:38:34 +0900

    Re-write interface contracts under @mstable for solidity-v0.6.12

--- contracts/@mstable/protocol/contracts/interfaces/IMStableHelper.sol
+++ contracts/@mstable/protocol/contracts/interfaces/IMStableHelper.sol
@@ -7,9 +7,9 @@ interface IMStableHelper {
     /**
      * @dev Returns a valid bAsset with which to mint
      * @param _mAsset Masset addr
-     * @return valid bool
-     * @return string message
-     * @return address of bAsset to mint
+     * return - valid bool
+     * return - string message
+     * return - address of bAsset to mint
      */
     function suggestMintAsset(
         address _mAsset
@@ -27,10 +27,10 @@ interface IMStableHelper {
      * @param _mAsset mAsset address (e.g. mUSD)
      * @param _input Asset to input only bAssets accepted
      * @param _output Either a bAsset or the mAsset
-     * @return valid
-     * @return validity reason
-     * @return max input units (in native decimals)
-     * @return how much output this input would produce (in native decimals, after any fee)
+     * return - valid
+     * return - validity reason
+     * return - max input units (in native decimals)
+     * return - how much output this input would produce (in native decimals, after any fee)
      */
     function getMaxSwap(
         address _mAsset,
@@ -50,9 +50,9 @@ interface IMStableHelper {
     /**
      * @dev Returns a valid bAsset to redeem
      * @param _mAsset Masset addr
-     * @return valid bool
-     * @return string message
-     * @return address of bAsset to redeem
+     * return - valid bool
+     * return - string message
+     * return - address of bAsset to redeem
      */
     function suggestRedeemAsset(
         address _mAsset
@@ -70,10 +70,10 @@ interface IMStableHelper {
      * @param _mAsset Address of the given mAsset (e.g. mUSD)
      * @param _mAssetQuantity Amount of mAsset to redeem (in mUSD units)
      * @param _outputBasset Desired output bAsset
-     * @return valid
-     * @return validity reason
-     * @return output in bAsset units
-     * @return bAssetQuantityArg - required input argument to the 'redeem' call
+     * return - valid
+     * return - validity reason
+     * return - output in bAsset units
+     * return - bAssetQuantityArg - required input argument to the 'redeem' call
      */
     function getRedeemValidity(
         address _mAsset,
@@ -93,7 +93,7 @@ interface IMStableHelper {
      * @dev Gets the users savings balance in Masset terms
      * @param _save SAVE contract address
      * @param _user Address of the user
-     * @return balance in Masset units
+     * return - balance in Masset units
      */
     function getSaveBalance(
         ISavingsContract _save,
@@ -110,7 +110,7 @@ interface IMStableHelper {
      * amount of Masset from the SAVE contract
      * @param _save SAVE contract address
      * @param _amount Amount of mAsset to redeem from SAVE
-     * @return input for the redeem function (ie. credit units to redeem)
+     * return - input for the redeem function (ie. credit units to redeem)
      */
     function getSaveRedeemInput(
         ISavingsContract _save,
--- contracts/@mstable/protocol/contracts/interfaces/IMasset.sol
+++ contracts/@mstable/protocol/contracts/interfaces/IMasset.sol
@@ -8,43 +8,44 @@ import { MassetStructs } from "../masset/shared/MassetStructs.sol";
  *        VERSION: 1.0
  *        DATE:    2020-05-05
  */
-contract IMasset is MassetStructs {
+abstract contract IMasset is MassetStructs {
+//contract IMasset is MassetStructs {
 
     /** @dev Calc interest */
-    function collectInterest() external returns (uint256 massetMinted, uint256 newTotalSupply);
+    function collectInterest() external virtual returns (uint256 massetMinted, uint256 newTotalSupply);
 
     /** @dev Minting */
     function mint(address _basset, uint256 _bassetQuantity)
-        external returns (uint256 massetMinted);
+        external virtual returns (uint256 massetMinted);
     function mintTo(address _basset, uint256 _bassetQuantity, address _recipient)
-        external returns (uint256 massetMinted);
+        external virtual returns (uint256 massetMinted);
     function mintMulti(address[] calldata _bAssets, uint256[] calldata _bassetQuantity, address _recipient)
-        external returns (uint256 massetMinted);
+        external virtual returns (uint256 massetMinted);
 
     /** @dev Swapping */
     function swap( address _input, address _output, uint256 _quantity, address _recipient)
-        external returns (uint256 output);
+        external virtual returns (uint256 output);
     function getSwapOutput( address _input, address _output, uint256 _quantity)
-        external view returns (bool, string memory, uint256 output);
+        external virtual view returns (bool, string memory, uint256 output);
 
     /** @dev Redeeming */
     function redeem(address _basset, uint256 _bassetQuantity)
-        external returns (uint256 massetRedeemed);
+        external virtual returns (uint256 massetRedeemed);
     function redeemTo(address _basset, uint256 _bassetQuantity, address _recipient)
-        external returns (uint256 massetRedeemed);
+        external virtual returns (uint256 massetRedeemed);
     function redeemMulti(address[] calldata _bAssets, uint256[] calldata _bassetQuantities, address _recipient)
-        external returns (uint256 massetRedeemed);
-    function redeemMasset(uint256 _mAssetQuantity, address _recipient) external;
+        external virtual returns (uint256 massetRedeemed);
+    function redeemMasset(uint256 _mAssetQuantity, address _recipient) external virtual;
 
     /** @dev Setters for the Manager or Gov to update module info */
-    function upgradeForgeValidator(address _newForgeValidator) external;
+    function upgradeForgeValidator(address _newForgeValidator) external virtual;
 
     /** @dev Setters for Gov to set system params */
-    function setSwapFee(uint256 _swapFee) external;
+    function setSwapFee(uint256 _swapFee) external virtual;
 
     /** @dev Getters */
-    function getBasketManager() external view returns(address);
-    function forgeValidator() external view returns (address);
-    function totalSupply() external view returns (uint256);
-    function swapFee() external view returns (uint256);
+    function getBasketManager() external virtual view returns(address);
+    function forgeValidator() external virtual view returns (address);
+    function totalSupply() external virtual view returns (uint256);
+    function swapFee() external virtual view returns (uint256);
 }
