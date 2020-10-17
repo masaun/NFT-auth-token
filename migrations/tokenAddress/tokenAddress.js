b49b2ccf9aff54127284220d40739a30c3d45ab2
masaun <masanoriuno@gmail.com>
2020-10-17 22:00:04 +0900

    Add migrations/2_deploy_pool-with-nft-auth-token.js for @mstable

--- migrations/tokenAddress/tokenAddress.js
+++ migrations/tokenAddress/tokenAddress.js
@@ -1,16 +1,13 @@
 module.exports = {
 	"Kovan": {
-		"DAI": "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa"    // DAI address on Kovan  
+        "mStable": {
+            "mUSD": "0x70605Bdd16e52c86FC7031446D995Cf5c7E1b0e7"
+        },
 	},
     "Ropsten": {
         "ETH": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",    // ETH address
         "DAI": "0xaD6D458402F60fD3Bd25163575031ACDce07538D",    // DAI address on Ropsten（from Kyber）
         "TUSD": "0xB36938c51c4f67e5E1112eb11916ed70A772bD75",        
         "LINK": "0x20fE562d797A42Dcb3399062AE9546cd06f63280",        
-    },
-    "Rinkeby": {
-        "DAI": "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa",    // DAI address on Rinkeby
-        "TUSD": "0x6F7454cBA97ffFE10e053187f23925a86F5C20C4",
-        "LINK": "0x01BE23585060835E02B77ef475b0Cc51aA1e0709",
-    },
+    }
 }
